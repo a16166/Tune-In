4cdcd71229280a4dd307cee3a1e76a9d3e675f31
a16166 <bernardof.santos@ua.pt>
2025-07-18 23:03:17 +0100

    (fix) added command handler and command deployment script for slash comamnds

---
+++ commands/utility/reload.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("reload")
+        .setDescription("Reloads a command.")
+        .addStringOption(option => 
+            option.setName("command").setDescription("the command to reload.").setRequired(true)),
+    async execute(interaction) {
+        const commandName = interaction.options.getString("command"); //parses StringOption
+        const command = interaction.client.commands.get(commandName);
+
+        if(!command) {
+            return await interaction.reply({ content: `There is no command with the name ${commandName}`, ephemeral: true });
+        }
+
+        delete require.cache[require.resolve(`./${command.data.name}.js`)];
+        try {
+            const newCommand = require(`./${commandName}`);
+            interaction.client.commands.set(newCommand.data.name, newCommand);
+            await interaction.reply(`Command \`${newCommand.data.name}\` was reloaded!`)
+
+        } catch (error) {
+            console.error(error);
+            await interaction.reply( {content: `There was an error while reloading a command \`${command.data.name}\`:\n\`${error.message}\``, ephemeral: true } )
+        }
+    }
+}
---
+++ commands/utility/serverinfo.js
@@ -0,0 +1,7 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("serverinfo")
+        .setDescription("Replies with server's info."),
+}
